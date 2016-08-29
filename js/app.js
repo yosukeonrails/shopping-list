3eafec48aa859b92335bc31d0e31aa19a17eace5
Yosuke Hishinuma <yosukeonrails@gmail.com>
2016-08-29 01:43:55 -0700

    working prototype

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
  
+
+ $(document).ready(function(){
+
+
+ 	$(".submit-cart").click(function(){
+      event.preventDefault();
+
+var inputval= $('.shopping-input').val()
  var newlist= `<li>
 
-        <span class="shopping-item">bread</span>
+        <span class="shopping-item">`+ inputval +
+
+        `</span>
         <div class="shopping-item-controls">
           <button class="shopping-item-toggle">
             <span class="button-label">check</span>
@@ -12,17 +22,30 @@
         </div>
 
       </li>`
-      
-
- $(document).ready(function(){
-
-
- 	$(".submit-cart").click(function(){
+    
 
  	   $(".shopping-list").append(newlist)
+
  	})
 
 
+    $(".shopping-list").on('click','.shopping-item-delete',function(){
+
+          this.closest("li")
+          .remove();
+    })
 
 
- })
+ //    $(".shopping-list").on('click', '.shopping-item-toggle', function(){
+ //        this.closest(".shopping-item")  
+ // })
+
+       
+    $(".shopping-list").on('click','.shopping-item-toggle',function(){
+
+     $(this).closest('li').find('.shopping-item').toggleClass("shopping-item__checked")
+
+})
+
+
+     })
